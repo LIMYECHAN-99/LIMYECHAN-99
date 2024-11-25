6b310dcfbb7f407f1cfa4981703b8346da3f44c5
LIMYECHAN-99 <yechan2485@naver.com>
2024-11-25 22:06:44 +0900

    test

--- app.py
+++ app.py
@@ -1,20 +1,7 @@
 from flask import Flask
-
 app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return '''
-        <html>
-        <head>
-            <title>My Homepage</title>
-        </head>
-        <body>
-            <h1>Welcome to My Homepage!</h1>
-            <p>This is a simple homepage served through GCP using Flask.</p>
-        </body>
-        </html>
-    '''
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+@app.route("/")
+def hello_world():
+ return "test page"
+if __name__ == "__main__":
+ app.run(debug=True, host="0.0.0.0", port=5000)
